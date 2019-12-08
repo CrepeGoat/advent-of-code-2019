341d6652b21edac48b58d838b02647d9c36ab412
CrepeGoat <awqatty.b@gmail.com>
2019-12-08 00:24:05 -0500

    implemented (most of) day2-part1 challenge
    TODO:
    - include last instruction in parsing

---
+++ day2/part1.rs
@@ -0,0 +1,76 @@
+use std::vec::Vec;
+
+fn exec_code(code: &mut Vec<usize>) {
+	let mut code_pos = 0usize;
+
+	while code_pos < code.len() {
+		let op = code[code_pos];
+		if op == 99usize {
+			break;
+		}
+
+		let arg1pos = code[code_pos+1];
+		let arg2pos = code[code_pos+2];
+		let respos = code[code_pos+3];
+
+		if op == 1usize {
+			code[respos] = code[arg1pos] + code[arg2pos]
+		}
+		else if op == 2usize {
+			code[respos] = code[arg1pos] * code[arg2pos]
+		}
+		else {
+			panic!("invalid opcode {:?}", op);
+		}
+
+		code_pos += 4;
+	}
+}
+
+//------------------------------------------------------------------
+
+fn parse_code_string(output: &mut Vec<usize>, input: &str) {
+	let mut word = String::new();
+
+	for ch in input.chars() {
+		if ch == ',' {
+			output.push(
+				word.trim().parse::<usize>().expect(
+					"invalid input string"
+				)
+			);
+			word.clear();
+		}
+		else {
+			word.push(ch);
+		}
+	}
+}
+
+fn print_code(code: &Vec<usize>) {
+	let mut iter = code.iter();
+
+	/*match iter.next() {
+		Some(expr) => print!("{:?}", expr),
+		None => {},
+	}
+	*/
+	for ch in iter {
+		print!("{:?},", ch);
+	}
+
+	println!("");
+}
+
+fn main() {
+	println!("hello");
+
+	let mut code = Vec::<usize>::new();
+	let mut buffer = String::new();
+	std::io::stdin().read_line(&mut buffer).expect("invalid code");
+	
+	parse_code_string(&mut code, &buffer);
+	exec_code(&mut code);
+
+	print_code(&code);
+}
