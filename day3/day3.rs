87a72f9034258c2e6a1337dff037f51377f73aec
CrepeGoat <awqatty.b@gmail.com>
2019-12-09 18:17:29 -0500

    added class `LineSegment`
    - represents an individual chunk of a wire's cartesian path
    - added `as_segments` method, to convert cartesian movements into path
      segments

--- day3/day3.rs
+++ day3/day3.rs
@@ -53,14 +53,48 @@ fn parse_sequence(output: &mut Vec<ManhattanMove>, input: &str) {
 
 //------------------------------------------------------------------
 
+#[derive(Debug, Clone, Copy)]
+struct Coordinate {
+	x: i64,
+	y: i64,
+}
+
+#[derive(Debug)]
+struct LineSegment (Coordinate, Coordinate);
+
+fn as_segments(output: &mut Vec<LineSegment>, input: &Vec<ManhattanMove>) {
+	let mut coord0: Coordinate;
+	let mut coord1 = Coordinate {x:0, y:0};
+
+	for movement in input.iter() {
+		coord0 = coord1;
+
+		match movement.orientation {
+			Orientation::Vertical => coord1.y += movement.distance,
+			Orientation::Horizontal => coord1.x += movement.distance,
+		};
+
+		output.push(LineSegment(coord0, coord1));
+	}
+}
+
+//------------------------------------------------------------------
+
 fn main() {
 	println!("Enter wire path:");
-	let mut move_seq = Vec::<ManhattanMove>::new();
+	let mut movements = Vec::<ManhattanMove>::new();
 	let mut buffer = String::new();
 	std::io::stdin().read_line(&mut buffer).expect("invalid code");
-	parse_sequence(&mut move_seq, &buffer);
+	parse_sequence(&mut movements, &buffer);
 
-	for i in move_seq.iter() {
+	for i in movements.iter() {
+		println!("{:?}", i);
+	}
+
+	let mut segments = Vec::<LineSegment>::new();
+	as_segments(&mut segments, &movements);
+
+	for i in segments.iter() {
 		println!("{:?}", i);
 	}
 }
