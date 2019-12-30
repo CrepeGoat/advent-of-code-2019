3f7b74cb1d59ba97e206ddd51969a4b2df9d7938
CrepeGoat <awqatty.b@gmail.com>
2019-12-30 08:45:09 -0500

    removed main function from intcode_comp.rs

--- day7/intcode_comp_d7.rs
+++ day7/intcode_comp_d7.rs
@@ -330,14 +330,3 @@ fn print_code(code: &Vec<i32>) {
 
 	println!("");
 }
-
-fn main() {
-	println!("Enter program code below:");
-	let mut program = Vec::<i32>::new();
-	let mut buffer = String::new();
-	std::io::stdin().read_line(&mut buffer).expect("invalid program");
-	parse_code_string(&mut program, &buffer);
-	buffer.clear();
-	
-	exec_program_over_stdio(program);
-}
