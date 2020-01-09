438a80417abda9dcf2bc785d56aaa58de158a9ed
CrepeGoat <awqatty.b@gmail.com>
2020-01-09 07:36:16 -0500

    added amplifier code, `run_amplifier`

--- day7/day7.rs
+++ day7/day7.rs
@@ -4,6 +4,20 @@ use intcode_comp_d7::*;
 use std::vec::Vec;
 
 
+fn run_amplifier(program: Vec<i32>, phase: u8, input_signal: i32) -> i32 {
+	if let YieldStates::Pause(program_state) = YieldStates::new(program) {
+		if let YieldStates::Input(program_state) = program_state.execute().unwrap() {
+			if let YieldStates::Input(program_state) = program_state.execute(phase.into()).unwrap() {
+				if let YieldStates::Output(program_state) = program_state.execute(input_signal).unwrap() {
+					return *program_state.get();
+				}
+			}
+		}
+	}
+	panic!();
+}
+
+
 fn main() {
 	println!("Enter program code below:");
 	let mut program = Vec::<i32>::new();
