d8550417e99653f505b28da46e43d0c6aaa930c8
CrepeGoat <awqatty.b@gmail.com>
2020-01-12 11:15:43 -0500

    added chained amplifier function, `run_amp_chain`

--- day7/day7.rs
+++ day7/day7.rs
@@ -2,6 +2,7 @@ mod intcode_comp_d7;
 use intcode_comp_d7::*;
 
 use std::vec::Vec;
+use std::iter::Iterator;
 
 
 fn run_amplifier(program: Vec<i32>, phase: u8, input_signal: i32) -> i32 {
@@ -15,6 +16,18 @@ fn run_amplifier(program: Vec<i32>, phase: u8, input_signal: i32) -> i32 {
 		}
 	}
 	panic!();
+}
+
+
+fn run_amp_chain<ITER>(program: Vec<i32>, phases: ITER, first_input: i32) -> i32
+where ITER: Iterator<Item = u8>
+{
+	let mut last_output = first_input;
+	for phase in phases {
+		last_output = run_amplifier(program.clone(), phase, last_output);
+	}
+
+	last_output
 }
 
 
