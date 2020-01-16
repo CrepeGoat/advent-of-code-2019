e39595c947785cc000e7597b9dff729e59738819
CrepeGoat <awqatty.b@gmail.com>
2020-01-16 18:24:19 -0500

    refactored `run_amplifier`

--- day7/day7.rs
+++ day7/day7.rs
@@ -6,16 +6,24 @@ use std::iter::Iterator;
 
 
 fn run_amplifier(program: Vec<i32>, phase: u8, input_signal: i32) -> i32 {
-	if let YieldStates::Pause(program_state) = YieldStates::new(program) {
-		if let YieldStates::Input(program_state) = program_state.execute().unwrap() {
-			if let YieldStates::Input(program_state) = program_state.execute(phase.into()).unwrap() {
-				if let YieldStates::Output(program_state) = program_state.execute(input_signal).unwrap() {
-					return *program_state.get();
-				}
-			}
-		}
-	}
-	panic!();
+	let program_state = match YieldStates::new(program) {
+		YieldStates::Pause(ps) => ps,
+		_ => panic!()
+	};
+	let program_state = match program_state.execute().unwrap() {
+		YieldStates::Input(ps) => ps,
+		_ => panic!()
+	};
+	let program_state = match program_state.execute(phase.into()).unwrap() {
+		YieldStates::Input(ps) => ps,
+		_ => panic!()
+	};
+	let program_state = match program_state.execute(input_signal).unwrap() {
+		YieldStates::Output(ps) => ps,
+		_ => panic!()
+	};
+
+	*program_state.get()
 }
 
 
